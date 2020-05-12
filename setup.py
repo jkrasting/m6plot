4adf2d18996d6bdc9289be401a5a57eb83e29e0e
John Krasting <John.Krasting@noaa.gov>
2020-05-12 13:56:27 -0400

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="m6plot",
+    version="0.0.0",
+    author="MOM6 Development Team",
+    description="Generic plotting routines for MOM6 output",
+    url="https://github.com/jkrasting/m6plot",
+    packages=setuptools.find_packages(),
+)
